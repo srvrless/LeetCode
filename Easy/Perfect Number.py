2adf162cf6c1a663f464ea18c56ed509b86d1637
YOUR_NAME <YOUR_EMAIL>
2023-03-27 14:19:51 +0300

    okay

--- Easy/Perfect Number.py
+++ Easy/Perfect Number.py
@@ -1,14 +1,4 @@
 class Solution:
 
-    def convertToBase7(self, n: int) -> str:
-        x = abs(n)
-        b = 7
-        if x == 0:
-            return '0'
-        digits = []
-        while x:
-            digits.append(str(x % b))
-            x //= b
-        if n < 0:
-            digits.append('-')
-        return ''.join(digits[::-1])
+    def checkPerfectNumber(self, num: int) -> bool:
+        return num in [6, 28, 496, 8128, 33550336]
