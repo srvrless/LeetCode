51d4b6e9965927746807b66dc79752c68f9a2ed4
YOUR_NAME <YOUR_EMAIL>
2023-06-12 21:10:23 +0300

    123

--- Easy/The K Weakest Rows in a Matrix.py
+++ Easy/The K Weakest Rows in a Matrix.py
@@ -1,15 +1,8 @@
-from collections import Counter
-mat = [[1,1,0,0,0],
- [1,1,1,1,0],
- [1,0,0,0,0],
- [1,1,0,0,0],
- [1,1,1,1,1]], 
-k = 3
-print()
-res=[]
-for i in range(len(mat[0])-1):
-    c=Counter(mat[0][i])
-    c1=Counter(mat[0][i+1])
-    if c.get(1) <c1.get(1):
-        res.append(i)
-print(res)
+from typing import List
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        counts = [(sum(row), i) for i, row in enumerate(mat)]
+        counts.sort()
+
+        return [i for _, i in counts[:k]]
